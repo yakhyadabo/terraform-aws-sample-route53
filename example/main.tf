3dbe80b0aa5c8753fa7956c04b730fd9f73b9b9a
Yakhya Dabo <yakhyadabo@gmail.com>
2023-02-27 21:40:28 +0000

    [example] Use complex params

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,21 @@
 
+locals {
+  domains = {
+    "example.org" = {
+      sub_domain_names = ["dev", "stage", "test"]
+    },
+    "example.com" = {
+      sub_domain_names = ["dev", "stage", "test"]
+    }
+  }
+}
+
 module "route53" {
+  for_each = local.domains
   source               = "../module"
   region               = var.region
   project_name         = var.project.name
   environment          = var.project.environment
-  root_domain          = var.root_domain
-  sub_domains           = var.sub_domains
+  root_domain          = each.key
+  sub_domains           = each.value.sub_domain_names
 }
